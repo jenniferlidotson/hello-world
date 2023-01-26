ab2e7ce019b85dc397bff41efd6f4d8b14143941
Jennifer Li Dotson <jenniferlidotson@gmail.com>
2023-01-25 20:40:48 -0800

    added dynamic components

---
+++ script.js
@@ -0,0 +1,20 @@
+let usersName = prompt("Aloha! What's' your first name?"); 
+console.log(userName);
+document.write("Aloha" + userName);
+
+let userInput = prompt("Do you love the ocean? Yes or No.");
+console.log(userInput);
+let myAnswer = Yes;
+
+if (userInput == myAnswer){
+document.write("We're glad you love the ocean! Join us in the water!");
+
+}else {
+document.write("Even if you don't love the ocean! You can learn more here!");
+
+if (confirm("Would you like to enter this website?")) {
+document.write("Awesome!");
+
+}else {
+document.write("No worries!");
+}
