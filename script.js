500baba7496642c04de5c093124ba0bb1c75f533
Jennifer Li Dotson <jenniferlidotson@gmail.com>
2023-01-25 21:31:29 -0800

    added console log

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-let usersName = prompt ("Aloha! What's' your first name?"); 
+console.log("hello-world")
+
+let userName = prompt("Aloha! What's' your first name?");
 console.log(userName);
-document.write("Aloha" + userName);
+document.write("Aloha " + userName+"! ");
 
-let userInput = prompt("Do you love the ocean? Yes or No.");
+let userInput = prompt("Do you love the ocean? yes or no.");
 console.log(userInput);
-let myAnswer = Yes;
+let myAnswer = "yes";
 
-if (userInput == myAnswer){
-document.write("We're glad you love the ocean! Join us in the water!");
+if (userInput == myAnswer) {
+    document.write("We're glad you love the ocean! Join us in the water! ");
 
-}else {
-document.write("Even if you don't love the ocean! You can learn more here!");
+} else {
+    document.write("Even if you don't love the ocean, you can learn more here. ");
+}
+if (confirm("Would you like to enter this website?")) {
+    document.write(" Awesome!");
 
-if (confirm("Would you like to enter this website?")){
-document.write("Awesome!");
-
-}else {
-document.write("No worries!");
+} else {
+    document.write("No worries!");
 }
